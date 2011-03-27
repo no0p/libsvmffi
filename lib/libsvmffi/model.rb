81ebf3f1a3ebe7b014db9aedb9090c1fe42a8508
robert berry <berrydigital@gmail.com>
2011-03-27 01:31:31 -0700

    a bit more model

--- lib/libsvmffi/model.rb
+++ lib/libsvmffi/model.rb
@@ -3,6 +3,43 @@ module Libsvmffi
   #
   #
   class Model
+ 
+    attr_accessor :parameters, :problem, :svm_model
+
+    def initialize(options = {})
+
+      @parameters = Parameters.new      
+      
+      # TODO merge defaults options hash.
+      @parameters[:svm_type] = C_SVC;
+      @parameters[:kernel_type] = RBF;
+      @parameters[:degree] = 3;
+      @parameters[:gamma] = 0;  # 1/num_features
+      @parameters[:coef0] = 0;
+      @parameters[:nu] = 0.5;
+      @parameters[:cache_size] = 100;
+      @parameters[:C] = 1;
+      @parameters[:eps] = 0.001;
+      @parameters[:p] = 0.1;
+      @parameters[:shrinking] = 1;
+      @parameters[:probability] = 0;
+      @parameters[:nr_weight] = 0;
+      @parameters[:weight_label] = nil;
+      @parameters[:weight] = nil;
+    end
+
+
+    def add(label, features)
+      
+    end
+
+    def train
     
+    end
+
+    def save
+
+    end
+   
   end
 end
