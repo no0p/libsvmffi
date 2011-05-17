777bd4926606ab41da4a9715e3634d85303a3977
robert berry <berrydigital@gmail.com>
2011-05-17 03:21:44 -0700

    well you can build a data structure now

--- lib/libsvmffi/model.rb
+++ lib/libsvmffi/model.rb
@@ -7,7 +7,7 @@ module Libsvmffi
     attr_accessor :parameters, :problem, :svm_model
     attr_accessor :examples
     attr_accessor :labels, :features 
-    attr_accessor :elements
+    attr_accessor :elements, :x_space
 
     def initialize(options = {})
 
@@ -56,22 +56,27 @@ module Libsvmffi
     def train
 
       @problem = Problem.new
-      @problem.l = @examples.length
-      @problem.y = @examples.keys
-      @problem.x = FFI::MemoryPointer.new(:pointer, @problem.l)
+      @problem[:l] = @examples.length
+      @problem[:y] = FFI::MemoryPointer.new(:double, @problem[:l])
+      @problem[:x] = FFI::MemoryPointer.new(:pointer, @problem[:l])
       @x_space = FFI::MemoryPointer.new(:pointer, @elements)
 
+      y = @examples.map {|e| e.keys.first}
+      @problem[:y].put_array_of_double 0, y
+
       space_index = 0
-      @examples.values.each_with_index do |e, i|
-        @problem.x[i] = @x_space[space_index].pointer
-      
-        e.merge!({-1 => 0}) #terminator
-        e.each do |k, v|
-          n = Node.new
-          n[:index] = k
-          n[:value] = v
-          @x_space[space_index] = n.pointer
-          space_index += 1
+      @examples.each do |ex| #TODO clean up this hash structure
+        ex.each_with_index do |e, i|
+          @problem[:x][i].put_pointer 0, @x_space[space_index]
+        
+          features = e.last.merge({-1 => 0}) #terminator
+          features.each do |k, v|
+            n = Node.new
+            n[:index] = k
+            n[:value] = v
+            @x_space[space_index].put_pointer 0, n.pointer
+            space_index += 1
+          end
         end
       end
       
